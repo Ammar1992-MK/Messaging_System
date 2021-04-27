9999ca5e73541187d1be1775e040193daa229c59
amkh <ammarkhalil08@gmail.com>
2021-04-27 19:58:22 +0200

    78% test coverage

--- _tests_/ChatView.test.jsx
+++ _tests_/ChatView.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Simulate} from 'react-dom/test-utils'
 import {ChatView} from "../src/client/ChatView";
 describe("Chat View",() => {
     it("shows messages in the chat log", () => {
@@ -8,5 +9,19 @@ describe("Chat View",() => {
         expect(container).toMatchSnapshot();
         expect(container.querySelector(".chat-log div").textContent).toEqual("good morning :)");
 
+    });
+
+    it("send new message in chat", () => {
+        const container = document.createElement("div");
+        const sendMessage = jest.fn();
+        ReactDOM.render(<ChatView chatLog={[]} sendMessage={sendMessage}/>, container);
+
+        Simulate.change(container.querySelector("input"), {
+            target : {value : "Hello chat"},
+        });
+
+        Simulate.submit(container.querySelector("form"));
+
+        expect(sendMessage).toBeCalledWith("Hello chat")
     })
 })
